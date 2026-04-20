1d238b2a3a594668183245f3106eedd93a64d3f6
Zookegger <nguyenductrung11122004@gmail.com>
2026-04-20 16:05:41 +0700

    chỉnh sửa: cập nhật các thay đổi mã nguồn

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from "react";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
+import CardWrapper from "@/app/ui/dashboard/cards";
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">Dashboard</h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton />} >
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+            </div>
+        </main>
+    )
+}
